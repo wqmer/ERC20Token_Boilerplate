22f81c78aa87b6e07376706d572de4a8d1c1e2fb
Kimmy <kimmy@MacBook-Pro.local>
2018-02-26 02:07:35 -0800

    hook with infura

--- app/test.js
+++ app/test.js
@@ -1,40 +1,11 @@
-
-
-
-const {balance} = require("./Token_detail");
-const {tokenName} = require("./Token_detail");
-const  DemoToken =  require("./Token_detail").tokenContract;
-const {tokenSymbol} = require("./Token_detail");
-
-var http = require('http');
-http.createServer(function(req,res){
-res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 var Web3 = require('web3');
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
-
-// var account_one = web3.eth.accounts[0];
-// var account_two = web3.eth.accounts[1];
-//var account_one_balance = DemoToken.getBalance.call(account_one);
+web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io/1BPk9syIaAtEQzRuW9uF'));
+const { generateMnemonic, EthHdWallet } = require('eth-hd-wallet');
 
 
-console.log(web3.eth.accounts[0]);
-//var balance = web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]),'ether');
-console.log(balance);
-//console.log(account_one_balance.toNumber());
-res.write('<h2>');
-res.write('MainAdress：');
-res.write('</h1>');
-res.write('<p>');
-res.write(web3.eth.accounts[0].toString());
-res.write('</p>');
-res.write('<h2>');
-res.write(tokenName + ':');
-res.write('</h1>');
-res.write('<p>');
-res.write(balance.toString() + '  ' + tokenSymbol );
-//res.write(account_one_balance.toString());
-res.write('</p>');
-res.end();
-}).listen(8000);//监听8000端口
-console.log('Server is running');
+console.log(web3.eth.accounts);
+
+var mnemonic = "spot plunge retire helmet skin notice furnace obtain hour cousin oxygen post";
+const wallet = EthHdWallet.fromMnemonic(mnemonic);
+console.log( wallet.generateAddresses(2) );
--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = "spot plunge retire helmet skin notice furnace obtain hour cousin oxygen post";
+
+
 module.exports = {
   networks: {
-  development: {
+   development: {
     host: "127.0.0.1",
     port: 7545,
     network_id: "*" // Match any network id
- }
+  },
+  ropsten: {
+    provider: function() {
+       return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/1BPk9syIaAtEQzRuW9uF")
+    },
+   network_id: 3 ,
+   gas : 2000000,
+   gasPrice : 1000000000
+  }   
+
 }
 };
